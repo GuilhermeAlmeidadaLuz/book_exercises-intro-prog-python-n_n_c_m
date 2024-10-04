7fb435ffdea63ad8ef98f3d4661557433bb99f01
Guilherme Almeida <107274359+GuilhermeAlmeidadaLuz@users.noreply.github.com>
2024-10-04 15:19:23 -0400

    adding exercises 2.3 until 2.6

---
+++ Cap 2/Pag_49_Ex_2.3-6/Pag_49_Ex_2.4_.py
@@ -0,0 +1,9 @@
+# Exercício 2.4 - Escreva um programa que exiba o resultado de 2a x 3b,
+# em que a vale 3 e b vale 5.
+
+a = 3
+b = 5
+expression = (2 * a) * (3 * b)
+print()
+print("The expression result is:", expression)
+print()
